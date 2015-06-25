d022421645458eb767ec39231a7e66c8d7cf9a77
Ameni Bouattour <bouattour.ameni@gmail.com>
2015-06-25 12:11:42 +0200

    change

--- platforms/android/assets/www/js/app.js
+++ platforms/android/assets/www/js/app.js
@@ -1,32 +1,25 @@
 angular.module('ionicApp', ['ionic','ngCordova', 'ngCordovaOauth'])
-.controller('SignInCtrl', function($scope, $state, $cordovaOauth) {
-  
-  $scope.signIn = function(user) {
 
-    
-    $cordovaOauth.facebook("838665296203217", ["email"]).then(function(result) {
+.run(function ($ionicPlatform, $rootScope, $cordovaOauth) {
+     $ionicPlatform.ready(function () {
+    // Hide the accessory bar by default (remove this to show the accessory bar          above the keyboard
+    // for form inputs)
+     console.log('run in')
+     
+     if(window.localStorage.getItem("id") == undefined)  {
+      console.log('if structure')
+             $cordovaOauth.facebook("838665296203217", ["email"]).then(function(result) {
          
-           window.localStorage.setItem("id", user.id);
-          window.localStorage.setItem("pwd", user.pwd);
-            $state.go('root.home');
-        }, function(error) {
+           window.localStorage.setItem("id", result.access_token);
+           console.log(result);
+           }, function(error) {
            alert('Facebook login failed: ' + error);
         });
-    console.log("sucess");
+
+         
   }
-  $scope.isSignIn = function(user) {
+}); 
 
-  /* if((window.localStorage.getItem("id") == undefined) || (window.localStorage.getItem("pwd") == undefined)) {
-            return true;
-        } else {
-            return false;
-        }*/
-        return false;
-  }
-});
+})
 
 
-var user = {
-  id : '',
-  pwd : ''
-}
